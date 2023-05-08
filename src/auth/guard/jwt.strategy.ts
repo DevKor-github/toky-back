9a3d921051defffecd35a63b674bc2e9fcb6b57a
yunz0926 <chldbswl9@gmail.com>
2023-05-08 23:45:48 +0900

    feat: auth guard

---
+++ src/auth/guard/jwt.strategy.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      secretOrKey: process.env.JWT_SECRET_KEY,
+      ignoreExpiration: false,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: any): Promise<any> {
+    //user validate
+  }
+}
