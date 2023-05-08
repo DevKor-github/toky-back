d628f830b60851d1537d64d7498c19b5da725a8e
yunz0926 <chldbswl9@gmail.com>
2023-05-08 23:49:04 +0900

    feat: get/post/patch betAnswer

--- src/bets/bets.controller.ts
+++ src/bets/bets.controller.ts
@@ -1,7 +1,66 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseEnumPipe,
+  Patch,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { BetsService } from './bets.service';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { CreateBetAnswerDto } from './dto/create-bet-answer.dto';
+import { UpdateBetAnswerDto } from './dto/update-bet-answer.dto';
+import { CurrentUser } from 'src/auth/decorator/current-user.decorator';
+import { UserEntity } from 'src/users/entities/user.entity';
+import { Match } from 'src/common/enums/event.enum';
+import { DefaultUserInterceptor } from 'src/auth/interceptor/default-user.interceptor';
 
 @Controller('bets')
 export class BetsController {
   constructor(private readonly betsService: BetsService) {}
+
+  @Get('/questions/:match')
+  @ApiOperation({ summary: '경기 별 베팅 항목 리스트 조회하기' })
+  async getBetQuestions(
+    @Param('match', new ParseEnumPipe(Match)) match: Match,
+  ) {
+    return this.betsService.getBetQuestions(match);
+  }
+
+  //@ApiBearerAuth()
+  @Post('/')
+  //@UseGuards(JwtAuthGuard)
+  @UseInterceptors(DefaultUserInterceptor)
+  @ApiOperation({ summary: '베팅 항목 별 첫 베팅하기' })
+  async createBetAnswer(
+    @Body() createBetAnswerDto: CreateBetAnswerDto,
+    @CurrentUser() user: UserEntity,
+  ) {
+    return this.betsService.createBetAnswer(user, createBetAnswerDto);
+  }
+
+  //@ApiBearerAuth()
+  @Get('/')
+  //@UseGuards(JwtAuthGuard)
+  @UseInterceptors(DefaultUserInterceptor)
+  @ApiOperation({ summary: '사용자가 베팅한 이력 조회하기' })
+  async getBetAnswers(@CurrentUser() user: UserEntity) {
+    return this.betsService.getBetAnswers(user.id);
+  }
+
+  //@ApiBearerAuth()
+  @Patch('/')
+  //@UseGuards(JwtAuthGuard)
+  @UseInterceptors(DefaultUserInterceptor)
+  @ApiOperation({ summary: '사용자 베팅 수정하기' })
+  async updateBetAnswer(
+    @Body() updateBetAnswer: UpdateBetAnswerDto,
+    @CurrentUser() user: UserEntity,
+  ) {
+    return this.betsService.updateBetAnswer(user.id, updateBetAnswer);
+  }
 }
--- src/bets/bets.service.ts
+++ src/bets/bets.service.ts
@@ -1,4 +1,92 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { BetAnswerEntity } from './entities/betAnswer.entity';
+import { UserEntity } from 'src/users/entities/user.entity';
+import { CreateBetAnswerDto } from './dto/create-bet-answer.dto';
+import { UpdateBetAnswerDto } from './dto/update-bet-answer.dto';
+import { BetQuestionEntity } from './entities/betQuestion.entity';
+import { Match } from 'src/common/enums/event.enum';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class BetsService {}
+export class BetsService {
+  constructor(
+    @InjectRepository(BetAnswerEntity)
+    private readonly betAnswerRepository: Repository<BetAnswerEntity>,
+    @InjectRepository(BetQuestionEntity)
+    private readonly betQuestionRepository: Repository<BetQuestionEntity>,
+  ) {}
+
+  async getBetQuestions(match: Match) {
+    return this.betQuestionRepository.find({
+      where: {
+        match,
+      },
+    });
+  }
+
+  async createBetAnswer(user: UserEntity, createDto: CreateBetAnswerDto) {
+    const { questionId, answer } = createDto;
+    console.log(questionId, answer);
+    const question = await this.betQuestionRepository.findOne({
+      where: {
+        id: questionId,
+      },
+    });
+
+    if (!question) {
+      throw new NotFoundException(
+        'An betting question with requested questionId does not exist',
+      );
+    }
+
+    const result = await this.betAnswerRepository.create({
+      user,
+      question,
+      answer,
+    });
+
+    try {
+      await this.betAnswerRepository.save(result);
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async getBetAnswers(userId: number) {
+    return this.betAnswerRepository.find({
+      where: {
+        user: {
+          id: userId,
+        },
+      },
+      relations: {
+        question: true,
+      },
+    });
+  }
+
+  async updateBetAnswer(userId: number, updateDto: UpdateBetAnswerDto) {
+    const { answerId, answer } = updateDto;
+    const betAnswer = await this.betAnswerRepository.findOne({
+      where: {
+        id: answerId,
+        user: { id: userId },
+      },
+    });
+
+    if (!betAnswer) {
+      throw new NotFoundException(
+        'An betting answer with requested answerId and user does not exist',
+      );
+    }
+
+    betAnswer.answer = answer;
+
+    try {
+      await this.betAnswerRepository.save(betAnswer);
+    } catch (e) {
+      throw e;
+    }
+  }
+}
