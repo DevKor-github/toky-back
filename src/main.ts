c981ab529f2123a59a05e156647fefd4ea724eb5
yunz0926 <chldbswl9@gmail.com>
2023-05-09 16:03:42 +0900

    feat: set swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,16 @@ async function bootstrap() {
       stopAtFirstError: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Toky Server')
+    .setDescription('Toky API Description')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
 }
