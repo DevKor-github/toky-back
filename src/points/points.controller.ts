7155ad32e30cadaea4ebea145a2caca7c23740ea
yunz0926 <chldbswl9@gmail.com>
2023-05-17 00:02:27 +0900

    feat: 경품 응모

--- src/points/points.controller.ts
+++ src/points/points.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, Get, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  UseInterceptors,
+} from '@nestjs/common';
 import { PointsService } from './points.service';
 import { ApiOperation } from '@nestjs/swagger';
 import { GetRankingQueryDto } from './dto/get-rank-query.dto';
+import { DefaultUserInterceptor } from 'src/auth/interceptor/default-user.interceptor';
+import { CurrentUser } from 'src/auth/decorator/current-user.decorator';
+import { UserEntity } from 'src/users/entities/user.entity';
+import { DrawGiftDto } from './dto/draw-gift.dto';
 
 @Controller('points')
 export class PointsController {
@@ -11,5 +22,15 @@ export class PointsController {
   @ApiOperation({ description: '항목 별 랭킹 조회' })
   async getRanking(@Query() query: GetRankingQueryDto) {
     return this.pointsService.getRanking(query.match);
+  }
+
+  @Post('/draw')
+  @UseInterceptors(DefaultUserInterceptor)
+  @ApiOperation({ description: '경품 응모' })
+  async drawForGift(
+    @CurrentUser() user: UserEntity,
+    @Body() drawGiftDto: DrawGiftDto,
+  ) {
+    return this.pointsService.drawForGift(drawGiftDto.giftId, user);
   }
 }
--- src/points/points.service.ts
+++ src/points/points.service.ts
@@ -1,14 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Match } from 'src/common/enums/event.enum';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { UserEntity } from 'src/users/entities/user.entity';
+import { GiftEntity } from './entities/gift.entity';
+import { DrawEntity } from './entities/draw.entity';
+import { HistoryEntity } from './entities/history.entity';
+import { Match } from 'src/common/enums/event.enum';
 
 @Injectable()
 export class PointsService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(GiftEntity)
+    private readonly giftRepository: Repository<GiftEntity>,
+    private readonly dataSource: DataSource,
   ) {}
   async getRanking(match?: Match) {
     const matchArr = [
@@ -75,5 +85,59 @@ export class PointsService {
         point: item.point[m],
       };
     });
+  }
+
+  async drawForGift(giftId: number, user: UserEntity) {
+    //해당 경품 번호가 있는지 확인
+    const gift = await this.giftRepository.findOne({
+      where: {
+        id: giftId,
+      },
+    });
+
+    if (!gift) {
+      throw new NotFoundException('Not found giftId');
+    }
+
+    const userWithPoint = await this.userRepository.findOne({
+      where: { id: user.id },
+      relations: {
+        point: true,
+      },
+    });
+
+    //remaining poit가 응모 포인트보다 큰지 확인
+    if (userWithPoint.point.remainingPoint < gift.requiredPoint) {
+      throw new BadRequestException('Not Enough point to draw');
+    }
+
+    //경품 응모 트랜잭션 생성
+    await this.dataSource
+      .transaction(async (manager) => {
+        //포인트 삭감
+        userWithPoint.point.remainingPoint -= gift.requiredPoint;
+
+        //응모 정보 생성
+        const draw = await manager.create(DrawEntity, {
+          user,
+          gift,
+        });
+
+        //포인트 내역 생성
+        const history = await manager.create(HistoryEntity, {
+          usage: 1,
+          usedPoint: gift.requiredPoint,
+          user,
+        });
+
+        await manager.save(userWithPoint);
+        await manager.save(draw);
+        await manager.save(history);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+
+    return;
   }
 }
