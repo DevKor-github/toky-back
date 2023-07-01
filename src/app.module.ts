530e8739f6938eeccb84a855bd0fc01f9cf07745
SeokWon <tjrdnjs1534@naver.com>
2023-07-01 22:01:39 +0900

    fix: synchronize false

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,7 @@ import { AuthModule } from './auth/auth.module';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       autoLoadEntities: true,
-      synchronize: true,
+      // synchronize: true,
     }),
     UsersModule,
     BetsModule,
