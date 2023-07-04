ab0bbaaf34e8ceb389ddbec1bd839644251d24d3
overthestream <njhnjh02@naver.com>
2023-07-05 00:06:03 +0900

    fix: lazy resolving

--- src/cheers/dto/cheer.dto.ts
+++ src/cheers/dto/cheer.dto.ts
@@ -1,12 +1,7 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum } from 'class-validator';
 import { University } from 'src/common/enums/university.enum';
 
 export class CheerDto {
   @IsEnum(University)
-  @ApiProperty({
-    description: '대학교',
-    type: University,
-  })
   univ: University;
 }
