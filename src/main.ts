deb07fb7267188f55da9f83b4361983e8dd7bae5
halionaz <samhsj@naver.com>
2024-08-22 22:36:24 +0900

    origin set reset & httpOnly false

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['https://dev.toky.devkor.club'],
-    credentials: true,
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(
     new ValidationPipe({
