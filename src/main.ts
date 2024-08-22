61b7d0566a120431e58f755b2aaab3f9b7f6e4a5
halionaz <samhsj@naver.com>
2024-08-22 22:29:05 +0900

    set origin (domain)

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: ['https://dev.toky.devkor.club'],
     credentials: true,
   });
 
