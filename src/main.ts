880c970bbaa05ea94ab0492ae76c23d71a9f0cfa
SeokWon <tjrdnjs1534@naver.com>
2024-08-22 23:16:13 +0900

    feat: localhost test

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000', // 프론트엔드 주소
+    credentials: true, // 쿠키 허용
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
