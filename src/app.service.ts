6ed110915255de6fdab5aea0e56913bc2c573a32
JeongYeonSeung <asas4654@daum.net>
2024-09-02 17:01:06 +0900

    test:: db 초기화

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! ci/cd Plz.';
+    return 'Hello World! db 초기화';
   }
 }
