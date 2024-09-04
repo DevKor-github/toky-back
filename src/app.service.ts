08b71767932a0c5f1b2e0d419a638068c0274ae0
JeongYeonSeung <asas4654@daum.net>
2024-09-05 02:06:13 +0900

    fix:: kakao client id change

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! 배포 레알 완 + 카카오 재설정';
+    return 'Hello World! kakao error fix';
   }
 }
