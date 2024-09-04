d9ff958ae844b1ec77e8048d5c465a5647689161
JeongYeonSeung <asas4654@daum.net>
2024-09-05 02:14:10 +0900

    fix:: domain change

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! kakao error fix';
+    return 'Hello World! kakao login error fix';
   }
 }
