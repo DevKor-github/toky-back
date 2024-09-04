689173e5a61a7837cd20064c395b285fe76005ad
JeongYeonSeung <asas4654@daum.net>
2024-09-05 00:38:53 +0900

    fix:: 진짜 최종 배포 테스트

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! 배포 찐찐찐최종 완';
+    return 'Hello World! 배포 레알 완';
   }
 }
