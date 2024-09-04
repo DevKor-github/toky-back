ba19921dde9126687a150092ef18d606880f43f8
JeongYeonSeung <asas4654@daum.net>
2024-09-05 01:58:53 +0900

    fix:: kakao client error 해결

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! 배포 레알 완';
+    return 'Hello World! 배포 레알 완 + 카카오 재설정';
   }
 }
