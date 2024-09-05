cce0c4ccae4bfe1ae141400307726d0ea7b45ae0
Devheun <dudwo5600@korea.ac.kr>
2024-09-05 17:26:57 +0900

    mod:: 출석체크 API 분리 - 기존에는 오늘의 퀴즈와 내 출석체크 현황들을 한번에 확인하는 API 사용 - 두 API를 분리하여 오늘의 퀴즈는 로그인 안해도 볼 수 있도록

--- src/attendance-check/dto/get-my-attendance.dto.ts
+++ src/attendance-check/dto/get-my-attendance.dto.ts
@@ -1,4 +1,4 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 import { AttendanceCheckEntity } from '../entities/attendance-check.entity';
 
 export class GetMyAttendanceResponseDto {
@@ -14,21 +14,12 @@ export class GetMyAttendanceResponseDto {
   }
 }
 
-export class GetAttendanceCheckQuizAndMyAttendanceResponseDto {
+export class GetAttendanceResponseDto {
   @ApiProperty({
     description: '출석한 날짜 & 출석한 날짜의 정답 여부',
     type: [GetMyAttendanceResponseDto],
   })
   attendanceHistory: GetMyAttendanceResponseDto[];
-
-  @ApiProperty({ description: '오늘 날짜 (YYYY-MM-DD 형식)' })
-  today: string;
-
-  @ApiProperty({ description: '출석체크 퀴즈 ID' })
-  quizId: number;
-
-  @ApiProperty({ description: '출석체크 퀴즈 질문' })
-  question: string;
 
   @ApiProperty({ description: '오늘 출석 여부' })
   todayAttendance: boolean;
@@ -41,17 +32,11 @@ export class GetAttendanceCheckQuizAndMyAttendanceResponseDto {
 
   constructor(
     attendanceHistory: GetMyAttendanceResponseDto[],
-    today: string,
-    quizId: number | null,
-    question: string | null,
     todayAttendance: boolean,
     isMyAnswerCorrect: boolean | null,
     todayAnswer: boolean | null,
   ) {
     this.attendanceHistory = attendanceHistory;
-    this.today = today;
-    this.quizId = quizId;
-    this.question = question;
     this.todayAttendance = todayAttendance;
     this.isMyAnswerCorrect = isMyAnswerCorrect;
     this.todayAnswer = todayAnswer;
